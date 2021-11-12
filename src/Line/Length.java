d51547796a92d00536bdf474d033d66e70e806de
tomthomas437@gmail.com <tomthomas437@gmail.com>
2021-11-12 20:58:50 +0530

    Length Calculation

---
+++ src/Line/Length.java
@@ -0,0 +1,30 @@
+package Line;
+import java.util.Scanner;
+
+public class Length {
+	public static void main(String arg[])
+	{
+	   int x1,x2,y1,y2;
+	   double dis;
+	   
+	   Scanner sc=new Scanner(System.in);
+	   
+	   System.out.println("enter x1 point");
+	        x1=sc.nextInt(); 
+       
+	   System.out.println("enter y1 point");
+            y1=sc.nextInt();
+ 
+	   System.out.println("enter x2point");   
+             x2=sc.nextInt();
+ 
+       System.out.println("enter y2 point");   
+              y2=sc.nextInt();
+	  	    
+		dis=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+ 	 	    	 	    
+		System.out.println("distance between"+"("+x1+","+y1+"),"+"("+x2+","+y2+") is : "+dis);
+ 
+	}
+ }
+
